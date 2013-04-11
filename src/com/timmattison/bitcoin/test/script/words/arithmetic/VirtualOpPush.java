fca4c798e2b23f2e027f2bea22894b60d1b7cbf4
Tim Mattison <tim.mattison@controlgroup.com>
2013-04-11 13:58:21 -0400

    Added virtual push opcode

--- src/com/timmattison/bitcoin/test/script/words/arithmetic/VirtualOpPush.java
+++ src/com/timmattison/bitcoin/test/script/words/arithmetic/VirtualOpPush.java
@@ -12,9 +12,17 @@ import com.timmattison.bitcoin.test.script.Word;
  */
 public class VirtualOpPush extends Word {
     private static final String word = "VIRTUAL_OP_PUSH";
+    private static final int MIN_OPCODE = 0x01;
+    private static final int MAX_OPCODE = 0x4B;
 
     public VirtualOpPush(int opcode) {
         super(word, opcode);
+
+        // Is the opcode valid?
+        if ((opcode < MIN_OPCODE) || (opcode > MAX_OPCODE)) {
+            // No, throw an exception
+            throw new UnsupportedOperationException("Opcode must be between " + MIN_OPCODE + " and " + MAX_OPCODE + ", saw " + opcode);
+        }
     }
 
     @Override
