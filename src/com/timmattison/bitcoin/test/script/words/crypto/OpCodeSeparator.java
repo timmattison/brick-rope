2e32cdf055514a2603aa80b31bb572f459f9062f
Tim Mattison <tim.mattison@controlgroup.com>
2013-04-11 13:41:27 -0400

    Added crypto opcodes

---
+++ src/com/timmattison/bitcoin/test/script/words/crypto/OpCodeSeparator.java
@@ -0,0 +1,25 @@
+package com.timmattison.bitcoin.test.script.words.crypto;
+
+import com.timmattison.bitcoin.test.script.StateMachine;
+import com.timmattison.bitcoin.test.script.Word;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timmattison
+ * Date: 4/11/13
+ * Time: 11:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpCodeSeparator extends Word {
+    private static final String word = "OP_CODESEPARATOR";
+    private static final int opcode = 0xab;
+
+    public OpCodeSeparator() {
+        super(word, opcode);
+    }
+
+    @Override
+    public void execute(StateMachine stateMachine) {
+        throw new UnsupportedOperationException();
+    }
+}
