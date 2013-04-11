89d66b958d894dda668f18ffc8f07fe72b014f6e
Tim Mattison <tim.mattison@controlgroup.com>
2013-04-11 14:52:17 -0400

    About to modify the word abstract class so it can consume bytes

--- src/com/timmattison/bitcoin/test/Script.java
+++ src/com/timmattison/bitcoin/test/Script.java
@@ -1,5 +1,11 @@
 package com.timmattison.bitcoin.test;
 
+import com.timmattison.bitcoin.test.script.Word;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: timmattison
@@ -8,4 +14,20 @@ package com.timmattison.bitcoin.test;
  * To change this template use File | Settings | File Templates.
  */
 public class Script {
+    private List<Byte> scriptByteList = new ArrayList<Byte>();
+    private List<Word> words;
+
+    public Script(Byte[] scriptBytes) {
+        scriptByteList = Arrays.asList(scriptBytes);
+
+        buildListOfWords();
+    }
+
+    private void buildListOfWords() {
+        words = new ArrayList<Word>();
+
+        while(!scriptByteList.isEmpty()) {
+            // Consume the bytes
+        }
+    }
 }
