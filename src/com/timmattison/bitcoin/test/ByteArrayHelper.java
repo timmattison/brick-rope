3b25e9c8717514a0984e21de31412b87b1254750
Tim Mattison <tim.mattison@controlgroup.com>
2013-04-24 23:09:10 -0400

    Removed some unused methods

--- src/com/timmattison/bitcoin/test/ByteArrayHelper.java
+++ src/com/timmattison/bitcoin/test/ByteArrayHelper.java
@@ -12,24 +12,6 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class ByteArrayHelper {
-    public static ArrayList<Byte> removeFirstByte(List<Byte> list) {
-        return new ArrayList<Byte>(list.subList(1, list.size()));
-    }
-
-    public static ArrayList<Byte> grabHeadBytes(List<Byte> list, int count) {
-        return new ArrayList<Byte>(list.subList(0, count));
-    }
-
-    public static ArrayList<Byte> grabTailBytes(List<Byte> list, int count) {
-        return new ArrayList<Byte>(list.subList(count, list.size()));
-    }
-
-    public static void addBytes(List<Byte> list, Byte[] bytes) {
-        for (int loop = 0; loop < bytes.length; loop++) {
-            list.add(bytes[loop]);
-        }
-    }
-
     public static byte[] JavaLangByteArrayToByteArray(Byte[] bytes) {
         byte[] returnValue = new byte[bytes.length];
 
@@ -44,20 +26,6 @@ public class ByteArrayHelper {
         Byte[] tempByteArray = bytes.toArray(new Byte[bytes.size()]);
 
         return JavaLangByteArrayToByteArray(tempByteArray);
-    }
-
-    public static Byte[] ByteArrayToJavaLangByteArray(byte[] bytes) {
-        Byte[] returnValue = new Byte[bytes.length];
-
-        for (int loop = 0; loop < bytes.length; loop++) {
-            returnValue[loop] = bytes[loop];
-        }
-
-        return returnValue;
-    }
-
-    public static List<Byte> JavaLangByteArrayToByteList(Byte[] bytes) {
-        return Arrays.asList(bytes);
     }
 
     public static void printArray(byte[] array) {
