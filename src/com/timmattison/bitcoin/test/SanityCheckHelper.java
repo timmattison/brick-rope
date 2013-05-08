8fc629e16d0f8af707142a786cc909057f3d3076
Tim Mattison <tim.mattison@controlgroup.com>
2013-05-08 16:35:56 -0400

    Added sanity check helper class

---
+++ src/com/timmattison/bitcoin/test/SanityCheckHelper.java
@@ -0,0 +1,33 @@
+package com.timmattison.bitcoin.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timmattison
+ * Date: 5/8/13
+ * Time: 7:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SanityCheckHelper {
+
+    public static void sanityCheckNotNull(Object input, String name) {
+        if (input == null) {
+            throw new UnsupportedOperationException(name + " cannot be NULL");
+        }
+    }
+
+    public static void sanityCheckByteArrayLengthEqual(byte[] input, int length, String name) {
+        sanityCheckNotNull(input, name);
+
+        if(input.length != length) {
+            throw new UnsupportedOperationException(name + " must have exactly " + length + " elements, has " + input.length);
+        }
+    }
+
+    public static void sanityCheckByteArrayLengthGreaterThan(byte[] input, int length, String name) {
+        sanityCheckNotNull(input, name);
+
+        if(input.length < length) {
+            throw new UnsupportedOperationException(name + " must have at least " + length + " elements, has " + input.length);
+        }
+    }
+}
