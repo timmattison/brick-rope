e041111c2e85c3e4183b34f995c8dea4ee1e3c52
Tim Mattison <tim.mattison@controlgroup.com>
2013-05-08 07:30:33 -0400

    Removed direct access to the stack, added methods to support code separator opcode and check sig opcode

--- src/com/timmattison/bitcoin/test/script/StateMachine.java
+++ src/com/timmattison/bitcoin/test/script/StateMachine.java
@@ -1,5 +1,7 @@
 package com.timmattison.bitcoin.test.script;
 
+import com.timmattison.bitcoin.test.ByteArrayHelper;
+
 import java.util.Stack;
 
 /**
@@ -10,11 +12,38 @@ import java.util.Stack;
  * To change this template use File | Settings | File Templates.
  */
 public class StateMachine {
-    public Stack<Object> stack = new Stack<Object>();
+    private Stack<Object> stack = new Stack<Object>();
+    private HashType hashType;
+    private byte hashTypeByte = 0;
+    private int currentPosition;
+
+    /**
+     * The position at which the last code separator was encountered
+     */
+    private int codeSeparatorPosition = -1;
+
+    /**
+     * The bytes for this script
+     */
+    private byte[] scriptBytes;
+
+    public Object pop() {
+        checkScriptBytesNotNull();
+
+        return stack.pop();
+    }
+
+    public void push(Object input) {
+        checkScriptBytesNotNull();
+
+        stack.push(input);
+    }
 
     public byte[] popByteArray() {
+
         Object item = stack.pop();
 
+        // TODO - Remove this Byte[] junk
         if (item instanceof Byte[]) {
             return convertByteArray((Byte[]) item);
         } else if (item instanceof byte[]) {
@@ -24,6 +53,7 @@ public class StateMachine {
         }
     }
 
+    // TODO - Remove this Byte[] junk
     public static byte[] convertByteArray(Byte[] input) {
         Byte[] array = (Byte[]) input;
         byte[] bytes = new byte[array.length];
@@ -32,5 +62,67 @@ public class StateMachine {
         }
 
         return bytes;
+    }
+
+    public HashType getHashType() {
+        checkScriptBytesNotNull();
+
+        // Is the hash type already set?
+        if(hashType == null) {
+            // No, determine which hash type we want
+            if(((hashTypeByte & 31) == HashType.SIGHASH_ALL.getValue())) {
+                // TODO - Check the logic on this.  It is missing from the docs.
+                hashType = HashType.SIGHASH_ALL;
+            }
+            else if((hashTypeByte & 31) == HashType.SIGHASH_NONE.getValue()) {
+                hashType = HashType.SIGHASH_NONE;
+            }
+            else if((hashTypeByte & 31) == HashType.SIGHASH_SINGLE.getValue()) {
+                hashType = HashType.SIGHASH_SINGLE;
+            }
+            else if((hashTypeByte & HashType.SIGHASH_ANYONECANPAY.getValue()) == HashType.SIGHASH_ANYONECANPAY.getValue()) {
+                hashType = HashType.SIGHASH_SINGLE;
+            }
+            else {
+                throw new UnsupportedOperationException("Unsupported hash type value found: " + hashTypeByte + " [" + ByteArrayHelper.toHex(hashTypeByte) + "]");
+            }
+        }
+
+        return hashType;
+    }
+
+    public void setHashTypeByte(byte hashTypeByte) {
+        checkScriptBytesNotNull();
+        this.hashTypeByte = hashTypeByte;
+        hashType = null;
+    }
+
+    public byte getHashTypeByte() {
+        checkScriptBytesNotNull();
+        return hashTypeByte;
+    }
+
+    public void setCodeSeparatorPosition() {
+        checkScriptBytesNotNull();
+        this.codeSeparatorPosition = currentPosition;
+    }
+
+    public void setScriptBytes(byte[] scriptBytes) {
+        this.scriptBytes = scriptBytes;
+    }
+
+    /**
+     * Check to make sure that the script bytes are not-NULL so script execution fails early if someone forgets to set
+     * the state machine up properly
+     */
+    private void checkScriptBytesNotNull() {
+        if(scriptBytes == null) {
+            throw new UnsupportedOperationException("Script bytes cannot be NULL");
+        }
+    }
+
+    public Object peek() {
+        checkScriptBytesNotNull();
+        return stack.peek();
     }
 }
