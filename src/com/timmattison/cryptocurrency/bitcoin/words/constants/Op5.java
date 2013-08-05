2d293d689459b72c54c7a0d5e678fbda805ff372
U-Tim-MSI\Tim <Tim@Tim-MSI.(none)>
2013-08-05 18:34:10 -0400

    asfd;

--- src/com/timmattison/cryptocurrency/bitcoin/words/constants/Op5.java
+++ src/com/timmattison/cryptocurrency/bitcoin/words/constants/Op5.java
@@ -1,7 +1,6 @@
 package com.timmattison.cryptocurrency.bitcoin.words.constants;
 
-import com.timmattison.bitcoin.test.script.StateMachine;
-import com.timmattison.bitcoin.test.script.Word;
+import com.timmattison.cryptocurrency.bitcoin.StateMachine;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,16 +9,22 @@ import com.timmattison.bitcoin.test.script.Word;
  * Time: 9:51 AM
  * To change this template use File | Settings | File Templates.
  */
-public class Op5 extends Word {
+public class Op5 extends ConstantOp {
     private static final String word = "OP_5";
     private static final Byte opcode = (byte) 0x55;
-
-    public Op5() {
-        super(word, opcode, false);
-    }
 
     @Override
     public void execute(StateMachine stateMachine) {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Byte getOpcode() {
+        return opcode;
+    }
+
+    @Override
+    public String getName() {
+        return word;
     }
 }
