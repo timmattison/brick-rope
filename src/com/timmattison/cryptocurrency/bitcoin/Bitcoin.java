9de1ca29d2cc74f0d30c8ae0ff21e845ef367c10
Tim Mattison <tim.mattison@controlgroup.com>
2013-08-05 11:48:57 -0400

    Now reading from a blockchain file pulled from Bitcoin-QT

--- src/com/timmattison/cryptocurrency/bitcoin/Bitcoin.java
+++ src/com/timmattison/cryptocurrency/bitcoin/Bitcoin.java
@@ -2,7 +2,12 @@ package com.timmattison.cryptocurrency.bitcoin;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.timmattison.cryptocurrency.interfaces.Block;
 import com.timmattison.cryptocurrency.interfaces.BlockChain;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,9 +17,15 @@ import com.timmattison.cryptocurrency.interfaces.BlockChain;
  * To change this template use File | Settings | File Templates.
  */
 public class Bitcoin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Injector injector = Guice.createInjector(new BitcoinModule());
 
         BlockChain blockChain = injector.getInstance(BlockChain.class);
+
+        File inputFile = new File("blockchain.dat");
+        FileInputStream inputStream = new FileInputStream(inputFile);
+
+        blockChain.setInputStream(inputStream);
+        Block block = blockChain.next();
     }
 }
