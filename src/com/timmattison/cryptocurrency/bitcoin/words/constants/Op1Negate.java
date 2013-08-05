8dccddbcc65e0affc7d5284fc504e6490823c215
U-Tim-MSI\Tim <Tim@Tim-MSI.(none)>
2013-08-05 18:33:31 -0400

    Partially implemented the constant ops

--- src/com/timmattison/cryptocurrency/bitcoin/words/constants/Op1Negate.java
+++ src/com/timmattison/cryptocurrency/bitcoin/words/constants/Op1Negate.java
@@ -1,7 +1,6 @@
 package com.timmattison.cryptocurrency.bitcoin.words.constants;
 
-import com.timmattison.bitcoin.test.script.StateMachine;
-import com.timmattison.bitcoin.test.script.Word;
+import com.timmattison.cryptocurrency.bitcoin.StateMachine;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,16 +9,22 @@ import com.timmattison.bitcoin.test.script.Word;
  * Time: 9:51 AM
  * To change this template use File | Settings | File Templates.
  */
-public class Op1Negate extends Word {
+public class Op1Negate extends ConstantOp {
     private static final String word = "OP_1NEGATE";
     private static final Byte opcode = (byte) 0x4f;
-
-    public Op1Negate() {
-        super(word, opcode, false);
-    }
 
     @Override
     public void execute(StateMachine stateMachine) {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Byte getOpcode() {
+        return opcode;
+    }
+
+    @Override
+    public String getName() {
+        return word;
     }
 }
