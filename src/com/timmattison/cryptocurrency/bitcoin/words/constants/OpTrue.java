61c5a47685c6ca49bf4efc8e729554813cc934f1
U-Tim-MSI\Tim <Tim@Tim-MSI.(none)>
2013-08-06 10:43:56 -0400

    Finished constant op bootstrapping

--- src/com/timmattison/cryptocurrency/bitcoin/words/constants/OpTrue.java
+++ src/com/timmattison/cryptocurrency/bitcoin/words/constants/OpTrue.java
@@ -1,7 +1,6 @@
 package com.timmattison.cryptocurrency.bitcoin.words.constants;
 
-import com.timmattison.bitcoin.test.script.StateMachine;
-import com.timmattison.bitcoin.test.script.Word;
+import com.timmattison.cryptocurrency.bitcoin.StateMachine;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,16 +9,22 @@ import com.timmattison.bitcoin.test.script.Word;
  * Time: 9:51 AM
  * To change this template use File | Settings | File Templates.
  */
-public class OpTrue extends Word {
+public class OpTrue extends ConstantOp {
     private static final String word = "OP_TRUE";
     private static final Byte opcode = (byte) 0x51;
-
-    public OpTrue() {
-        super(word, opcode, false);
-    }
 
     @Override
     public void execute(StateMachine stateMachine) {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Byte getOpcode() {
+        return opcode;
+    }
+
+    @Override
+    public String getName() {
+        return word;
     }
 }
