6db439297d35a3ea3193856bfc431d20b0e4b304
Tim Mattison <tim.mattison@controlgroup.com>
2013-08-08 11:32:56 -0400

    Added HashComparatorTest

---
+++ src/com/timmattison/cryptocurrency/standard/tests/TestHelper.java
@@ -0,0 +1,11 @@
+package com.timmattison.cryptocurrency.standard.tests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timmattison
+ * Date: 8/8/13
+ * Time: 11:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestHelper {
+}
