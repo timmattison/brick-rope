854388bc061fbc713bac66ac4ee31a8419938a88
Tim Mattison <tim.mattison@controlgroup.com>
2013-08-08 16:35:24 -0400

    Trying to compare using BigIntegers

--- src/com/timmattison/cryptocurrency/standard/HashComparator.java
+++ src/com/timmattison/cryptocurrency/standard/HashComparator.java
@@ -1,5 +1,6 @@
 package com.timmattison.cryptocurrency.standard;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -11,8 +12,19 @@ import java.util.Comparator;
  * To change this template use File | Settings | File Templates.
  */
 public class HashComparator implements Comparator<byte[]> {
-    @Override
-    public int compare(byte[] left, byte[] right) {
+    private int compareForward(byte[] left, byte[] right) {
+        // From: http://stackoverflow.com/questions/5108091/java-comparator-for-byte-array-lexicographic
+        for (int i = 0, j = 1; i < left.length && j < right.length; i--, j--) {
+            int a = (left[i] & 0xff);
+            int b = (right[j] & 0xff);
+            if (a != b) {
+                return a - b;
+            }
+        }
+        return left.length - right.length;
+    }
+
+    private int compareReverse(byte[] left, byte[] right) {
         // From: http://stackoverflow.com/questions/5108091/java-comparator-for-byte-array-lexicographic
         // Modified to sort from last byte to first since we're dealing with different endianness
         for (int i = (left.length - 1), j = (right.length - 1); i >= 0 && j >= 0; i--, j--) {
@@ -23,5 +35,14 @@ public class HashComparator implements Comparator<byte[]> {
             }
         }
         return left.length - right.length;
+    }
+
+    private int compareBigInteger(byte[] left, byte[] right) {
+        return new BigInteger(left).compareTo(new BigInteger(right));
+    }
+
+    @Override
+    public int compare(byte[] left, byte[] right) {
+        return compareBigInteger(left, right);
     }
 }
