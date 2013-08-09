1616749fe6e03b66aa51b7a0efb5905353427cbf
Tim Mattison <tim@mattison.org>
2013-08-09 07:20:21 -0400

    Removed unused comparator code

--- src/com/timmattison/cryptocurrency/standard/HashComparator.java
+++ src/com/timmattison/cryptocurrency/standard/HashComparator.java
@@ -13,18 +13,6 @@ import java.util.Comparator;
  * To change this template use File | Settings | File Templates.
  */
 public class HashComparator implements Comparator<byte[]> {
-    private int compareForward(byte[] left, byte[] right) {
-        // From: http://stackoverflow.com/questions/5108091/java-comparator-for-byte-array-lexicographic
-        for (int i = 0, j = 1; i < left.length && j < right.length; i--, j--) {
-            int a = (left[i] & 0xff);
-            int b = (right[j] & 0xff);
-            if (a != b) {
-                return a - b;
-            }
-        }
-        return left.length - right.length;
-    }
-
     private int compareReverse(byte[] left, byte[] right) {
         // From: http://stackoverflow.com/questions/5108091/java-comparator-for-byte-array-lexicographic
         // Modified to sort from last byte to first since we're dealing with different endianness
@@ -36,14 +24,6 @@ public class HashComparator implements Comparator<byte[]> {
             }
         }
         return left.length - right.length;
-    }
-
-    private int compareBigIntegerReversed(byte[] left, byte[] right) {
-        return new BigInteger(ByteArrayHelper.reverseBytes(left)).compareTo(new BigInteger(ByteArrayHelper.reverseBytes(right)));
-    }
-
-    private int compareBigInteger(byte[] left, byte[] right) {
-        return new BigInteger(left).compareTo(new BigInteger(right));
     }
 
     @Override
