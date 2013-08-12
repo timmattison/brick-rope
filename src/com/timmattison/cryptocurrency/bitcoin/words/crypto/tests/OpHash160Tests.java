cf366ba455cfe9b182e0709fa6f498ade8abb31c
Tim Mattison <tim@mattison.org>
2013-08-12 19:20:29 -0400

    Need some tests to validate that this works

---
+++ src/com/timmattison/cryptocurrency/bitcoin/words/crypto/tests/OpHash160Tests.java
@@ -0,0 +1,12 @@
+package com.timmattison.cryptocurrency.bitcoin.words.crypto.tests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tim
+ * Date: 8/12/13
+ * Time: 7:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpHash160Tests {
+    WRITE SOME TESTS
+}
