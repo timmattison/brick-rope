bf50b92f2819ade9ebd809ef212ffd92f18e1f79
Tim Mattison <tim.mattison@controlgroup.com>
2013-08-12 10:11:44 -0400

    Added Litecoin support (not finished yet, obviously!)

---
+++ src/com/timmattison/cryptocurrency/litecoin/LitecoinModule.java
@@ -0,0 +1,44 @@
+package com.timmattison.cryptocurrency.litecoin;
+
+import com.google.inject.AbstractModule;
+import com.timmattison.cryptocurrency.bitcoin.*;
+import com.timmattison.cryptocurrency.bitcoin.factories.*;
+import com.timmattison.cryptocurrency.factories.*;
+import com.timmattison.cryptocurrency.interfaces.*;
+import com.timmattison.cryptocurrency.standard.StandardBlockChain;
+import com.timmattison.cryptocurrency.standard.StandardMerkleRootCalculator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timmattison
+ * Date: 7/31/13
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LitecoinModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(BlockReader.class).to(LitecoinBlockReader.class);
+
+        bind(BlockChain.class).to(StandardBlockChain.class);
+        bind(Block.class).to(BitcoinBlock.class);
+        bind(BlockHeader.class).to(BitcoinBlockHeader.class);
+        bind(Block.class).to(BitcoinBlock.class);
+        bind(StateMachine.class).to(BitcoinStateMachine.class);
+        bind(BlockValidator.class).to(BitcoinBlockValidator.class);
+
+        bind(TransactionFactory.class).to(BitcoinTransactionFactory.class);
+        bind(BlockFactory.class).to(BitcoinBlockFactory.class);
+        bind(BlockHeaderFactory.class).to(BitcoinBlockHeaderFactory.class);
+        bind(InputFactory.class).to(BitcoinInputFactory.class);
+        bind(OutputFactory.class).to(BitcoinOutputFactory.class);
+        bind(ScriptFactory.class).to(BitcoinScriptFactory.class);
+        bind(WordFactory.class).to(BitcoinWordFactory.class);
+        bind(TargetFactory.class).to(BitcoinTargetFactory.class);
+
+        bind(HasherFactory.class).to(DoubleSha256Factory.class);
+
+        bind(MerkleRootCalculator.class).to(StandardMerkleRootCalculator.class);
+        //bind(MerkleRootCalculator.class).to(ParallelMerkleRootCalculator.class);
+    }
+}
