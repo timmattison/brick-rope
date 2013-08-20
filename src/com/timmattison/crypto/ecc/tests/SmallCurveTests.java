862e5f3aaa350d160f156b1948a1cc8159036f59
Tim Mattison <tim@mattison.org>
2013-08-20 19:44:14 -0400

    Elliptic curve Diffie-Hellman key exchange works on small curve

--- src/com/timmattison/crypto/ecc/tests/SmallCurveTests.java
+++ src/com/timmattison/crypto/ecc/tests/SmallCurveTests.java
@@ -8,7 +8,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigInteger;
-import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -36,6 +35,10 @@ public class SmallCurveTests {
         return eccPointFactory.create(curve, xFieldElement, yFieldElement);
     }
 
+    private ECCPoint getBasePoint() {
+        return getPoint(new BigInteger("5"), new BigInteger("1"));
+    }
+
     @Test
     public void testMultiplyInfinity() throws Exception {
         ECCParameters smallCurve = getSmallCurve1();
@@ -45,17 +48,30 @@ public class SmallCurveTests {
     }
 
     @Test
-    public void testMultiply1() {
-        ECCPoint firstPoint = getPoint(new BigInteger("5"),new BigInteger("1"));
+    public void testECDH1() {
+        ECCPoint basePoint = getBasePoint();
 
-        ECCPoint thirteenthPoint = firstPoint.multiply(BigInteger.valueOf(13));
+        BigInteger alicePrivateKey = BigInteger.valueOf(3);
+        ECCPoint alicePublicKey = basePoint.multiply(alicePrivateKey);
+
+        BigInteger bobPrivateKey = BigInteger.valueOf(10);
+        ECCPoint bobPublicKey = basePoint.multiply(bobPrivateKey);
+
+        ECCPoint aliceJointSecret = bobPublicKey.multiply(alicePrivateKey);
+        ECCPoint bobJointSecret = alicePublicKey.multiply(bobPrivateKey);
+
+        Assert.assertTrue(aliceJointSecret.equals(bobJointSecret));
+    }
+
+    @Test
+    public void testMultiply13P() {
+        ECCPoint thirteenthPoint = getBasePoint().multiply(BigInteger.valueOf(13));
         validatePoint(thirteenthPoint, 16, 4);
     }
 
     @Test
-    public void testDoubleFirstPoint() {
-        ECCPoint firstPoint = getPoint(new BigInteger("5"),new BigInteger("1"));
-        ECCPoint result = firstPoint.twice();
+    public void testDoubleBasePoint() {
+        ECCPoint result = getBasePoint().twice();
 
         Assert.assertTrue(result.getX().toBigInteger().equals(new BigInteger("6")));
         Assert.assertTrue(result.getY().toBigInteger().equals(new BigInteger("3")));
@@ -68,80 +84,80 @@ public class SmallCurveTests {
 
     @Test
     public void testCalculateUpTo18P() {
-        ECCPoint firstPoint = getPoint(new BigInteger("5"),new BigInteger("1"));
-        ECCPoint nextPoint = firstPoint.twice();
+        ECCPoint basePoint = getBasePoint();
+        ECCPoint nextPoint = getBasePoint().twice();
 
         // Test 3P: 10, 6
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 10, 6);
 
         // Test 4P: 3, 1
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 3, 1);
 
         // Test 5P: 9, 16
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 9, 16);
 
         // Test 6P: 16, 13
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 16, 13);
 
         // Test 7P: 0, 6
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 0, 6);
 
         // Test 8P: 13, 7
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 13, 7);
 
         // Test 9P: 7, 6
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 7, 6);
 
         // Test 10P: 7, 11
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 7, 11);
 
         // Test 11P: 13, 10
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 13, 10);
 
         // Test 12P: 0, 11
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 0, 11);
 
         // Test 13P: 16, 4
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 16, 4);
 
         // Test 14P: 9, 1
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 9, 1);
 
         // Test 15P: 3, 16
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 3, 16);
 
         // Test 16P: 10, 11
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 10, 11);
 
         // Test 17P: 6, 14
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 6, 14);
 
         // Test 18P: 5, 16
-        nextPoint = nextPoint.add(firstPoint);
+        nextPoint = nextPoint.add(basePoint);
         validatePoint(nextPoint, 5, 16);
     }
 
     @Test
     public void testCalculate19P() {
-        ECCPoint firstPoint = getPoint(new BigInteger("5"),new BigInteger("1"));
+        ECCPoint firstPoint = getPoint(new BigInteger("5"), new BigInteger("1"));
         ECCPoint nextPoint = firstPoint.twice();
 
-        for(int loop = 3; loop < 19; loop++) {
+        for (int loop = 3; loop < 19; loop++) {
             nextPoint = nextPoint.add(firstPoint);
         }
 
@@ -152,7 +168,7 @@ public class SmallCurveTests {
 
     @Test
     public void testCalculateFourthPoint() {
-        ECCPoint firstPoint = getPoint(new BigInteger("5"),new BigInteger("1"));
+        ECCPoint firstPoint = getPoint(new BigInteger("5"), new BigInteger("1"));
         ECCPoint secondPoint = firstPoint.twice();
         ECCPoint thirdPoint = secondPoint.add(firstPoint);
         ECCPoint fourthPoint = thirdPoint.add(firstPoint);
