9796aeacf1321954d7a28de3a5714e5dcafa950a
Tim Mattison <tim@mattison.org>
2013-08-27 07:15:58 -0400

    Added tests for decodePointHex

--- src/com/timmattison/crypto/ecc/tests/ECCurveFpTests.java
+++ src/com/timmattison/crypto/ecc/tests/ECCurveFpTests.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 public class ECCurveFpTests {
     Injector injector = Guice.createInjector(new ECCTestModule());
 
-   @Test
+    @Test
     public void testInfinityIsInfinity() {
         ECCParameters eccParameters = ECCTestHelper.getSmallCurve1Parameters(injector);
         ECCCurve eccCurve = eccParameters.getCurve();
@@ -38,12 +38,60 @@ public class ECCurveFpTests {
     }
 
     @Test
-    public void testShouldNotEqual() {
+    public void testShouldNotEqual1() {
         ECCParameters eccParameters1 = ECCTestHelper.getSmallCurve1Parameters(injector);
         ECCParameters eccParameters2 = ECCTestHelper.getSmallCurve2Parameters(injector);
         ECCCurve eccCurve1 = eccParameters1.getCurve();
         ECCCurve eccCurve2 = eccParameters2.getCurve();
 
         Assert.assertNotEquals(eccCurve1, eccCurve2);
+    }
+
+    @Test
+    public void testShouldNotEqual2() {
+        ECCParameters eccParameters1 = ECCTestHelper.getSmallCurve1Parameters(injector);
+        ECCCurve eccCurve1 = eccParameters1.getCurve();
+
+        Assert.assertFalse(eccCurve1.equals(eccParameters1));
+    }
+
+    @Test
+    public void testShouldReturnInfinity() {
+        ECCParameters eccParameters1 = ECCTestHelper.getSmallCurve1Parameters(injector);
+        ECCCurve eccCurve1 = eccParameters1.getCurve();
+
+        Assert.assertEquals(eccCurve1.getInfinity(), eccCurve1.decodePointHex("00"));
+    }
+
+    @Test
+    public void testShouldReturnNull1() {
+        ECCParameters eccParameters1 = ECCTestHelper.getSmallCurve1Parameters(injector);
+        ECCCurve eccCurve1 = eccParameters1.getCurve();
+
+        Assert.assertNull(eccCurve1.decodePointHex("02"));
+    }
+
+    @Test
+    public void testShouldReturnNull2() {
+        ECCParameters eccParameters1 = ECCTestHelper.getSmallCurve1Parameters(injector);
+        ECCCurve eccCurve1 = eccParameters1.getCurve();
+
+        Assert.assertNull(eccCurve1.decodePointHex("03"));
+    }
+
+    @Test
+    public void testShouldReturnNull3() {
+        ECCParameters eccParameters1 = ECCTestHelper.getSmallCurve1Parameters(injector);
+        ECCCurve eccCurve1 = eccParameters1.getCurve();
+
+        Assert.assertNull(eccCurve1.decodePointHex("05"));
+    }
+
+    @Test
+    public void testShouldReturnNull4() {
+        ECCParameters eccParameters1 = ECCTestHelper.getSmallCurve1Parameters(injector);
+        ECCCurve eccCurve1 = eccParameters1.getCurve();
+
+        Assert.assertNull(eccCurve1.decodePointHex("08"));
     }
 }
