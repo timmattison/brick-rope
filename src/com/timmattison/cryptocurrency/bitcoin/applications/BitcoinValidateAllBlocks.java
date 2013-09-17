4e91c689ab6a15d77ea5a66090e7f04dec20ff8d
Tim Mattison <tim@mattison.org>
2013-09-17 07:21:53 -0400

    Now looping through all blocks to see where the system fails

---
+++ src/com/timmattison/cryptocurrency/bitcoin/applications/BitcoinValidateAllBlocks.java
@@ -0,0 +1,91 @@
+package com.timmattison.cryptocurrency.bitcoin.applications;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.timmattison.bitcoin.test.ByteArrayHelper;
+import com.timmattison.cryptocurrency.bitcoin.BitcoinModule;
+import com.timmattison.cryptocurrency.bitcoin.StateMachine;
+import com.timmattison.cryptocurrency.factories.BlockChainFactory;
+import com.timmattison.cryptocurrency.factories.ScriptingFactory;
+import com.timmattison.cryptocurrency.factories.StateMachineFactory;
+import com.timmattison.cryptocurrency.interfaces.*;
+import com.timmattison.cryptocurrency.standard.Script;
+import com.timmattison.cryptocurrency.standard.ValidationScript;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timmattison
+ * Date: 7/31/13
+ * Time: 5:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BitcoinValidateAllBlocks {
+    public static ValidationScript validationScript;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ApplicationHelper.logFine();
+
+        Injector injector = Guice.createInjector(new BitcoinModule());
+
+        BlockChain blockChain = injector.getInstance(BlockChainFactory.class).getBlockChain();
+        TransactionLocator transactionLocator = injector.getInstance(TransactionLocator.class);
+
+        Block block;
+        int blockNumber = -1;
+
+        while ((block = blockChain.next()) != null) {
+            blockNumber++;
+
+            // Does this block have any non-coinbase transactions?
+            if (block.getTransactions().size() == 1) {
+                // No, just continue
+                continue;
+            }
+
+            // Loop through the non-coinbase transactions and prove they are valid
+            System.out.println(block.prettyDump(0));
+            System.out.println(ByteArrayHelper.toHex(block.dump()));
+
+            System.out.println("Transaction bytes:");
+            System.out.println(ByteArrayHelper.toHex(block.getTransactions().get(0).dump()));
+            System.out.println("Output bytes:");
+            System.out.println(ByteArrayHelper.toHex(block.getTransactions().get(0).getOutputs().get(0).dump()));
+            // This is the first block that has more than one transaction.
+
+            Transaction currentTransaction = block.getTransactions().get(1);
+
+            // Get its inputs
+            List<Input> inputs = currentTransaction.getInputs();
+
+            Input input = inputs.get(0);
+            String previousTransactionHash = ByteArrayHelper.toHex(input.getPreviousTransactionId());
+
+            long previousOutputIndex = input.getPreviousOutputIndex();
+
+            // Get the previous transaction
+            Transaction previousTransaction = transactionLocator.findTransaction(input.getPreviousTransactionId());
+
+            // Get the output
+            Output previousOutput = previousTransaction.getOutputs().get((int) previousOutputIndex);
+
+            // Get the input script
+            Script inputScript = input.getScript();
+
+            // Get the output script
+            Script outputScript = previousOutput.getScript();
+
+            validationScript = injector.getInstance(ScriptingFactory.class).createValidationScript(inputScript, outputScript);
+
+            StateMachine stateMachine = injector.getInstance(StateMachineFactory.class).createStateMachine();
+            stateMachine.setPreviousTransactionHash(input.getPreviousTransactionId());
+            stateMachine.setCurrentTransactionHash(block.getTransactions().get(1).getHash());
+
+            stateMachine.execute(validationScript);
+
+            return;
+        }
+    }
+}
