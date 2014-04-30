aa5d4ff2bc86b55065d7f642f06206664c06d7b1
Tim Mattison <tim.mattison@controlgroup.com>
2014-04-30 09:36:43 -0400

    Added curve serialization test, fails

--- src/test/com/timmattison/ecc/serialization/ECSerializationTests.java
+++ src/test/com/timmattison/ecc/serialization/ECSerializationTests.java
@@ -1,7 +1,23 @@
 package com.timmattison.ecc.serialization;
 
+import com.google.gson.Gson;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.timmattison.crypto.ecc.interfaces.ECCParameters;
+import com.timmattison.crypto.modules.ECCSECTestModule;
+import com.timmattison.ecc.ECCTestHelper;
+import org.junit.Test;
+
 /**
  * Created by timmattison on 4/30/14.
  */
 public class ECSerializationTests {
+    Injector injector = Guice.createInjector(new ECCSECTestModule());
+
+    @Test
+    public void testECCParametersCanBeSerialized() {
+        ECCParameters secp256k1 = ECCTestHelper.getSecp256k1(injector);
+        Gson gson = new Gson();
+        gson.toJson(secp256k1);
+    }
 }
