f2a43652a04f614c900de94bfb4be278c9d9303e
Tim Mattison <tim.mattison@controlgroup.com>
2014-05-07 08:04:26 -0400

    Working on H2 dump

---
+++ src/main/java/com/timmattison/cryptocurrency/bitcoin/applications/BitcoinDumpBlockChainToH2.java
@@ -0,0 +1,61 @@
+package com.timmattison.cryptocurrency.bitcoin.applications;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.timmattison.cryptocurrency.factories.BlockChainFactory;
+import com.timmattison.cryptocurrency.interfaces.Block;
+import com.timmattison.cryptocurrency.interfaces.BlockChain;
+import com.timmattison.cryptocurrency.modules.BitcoinModule;
+import com.timmattison.cryptocurrency.standard.BlockStorage;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timmattison
+ * Date: 7/31/13
+ * Time: 5:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BitcoinDumpBlockChainToH2 {
+    private static boolean debug = false;
+
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        ApplicationHelper.logFine();
+
+        Injector injector = Guice.createInjector(new BitcoinModule());
+
+        BlockChain blockChain = injector.getInstance(BlockChainFactory.class).getBlockChain();
+        BlockStorage blockStorage = injector.getInstance(BlockStorage.class);
+
+        Block block = blockChain.next();
+        int blockNumber = 0;
+
+        long start = new Date().getTime();
+
+        while (block != null) {
+            if((blockNumber % 1000) == 0) {
+                long timestamp = new Date().getTime();
+                System.out.println((timestamp - start) + ", " + blockNumber);
+            }
+
+            blockStorage.storeBlock(blockNumber, block);
+
+            if(debug) {
+                Block fromDb = blockStorage.getBlock(blockNumber);
+            }
+
+            block = blockChain.next();
+            blockNumber++;
+        }
+
+        //    BlockHeader blockHeader = block.getBlockHeader();
+        //    List<Transaction> transactions = block.getTransactions();
+        //    Output firstOutput = transactions.get(0).getOutputs().get(0);
+
+        //    StateMachine stateMachine = injector.getInstance(StateMachine.class);
+        //    stateMachine.execute(firstOutput.getScript());
+    }
+}
