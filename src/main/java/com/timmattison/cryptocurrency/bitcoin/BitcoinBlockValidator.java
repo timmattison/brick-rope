ab73b8928da0236ec883bbf0201bf3587e70eed4
Tim Mattison <tim@mattison.org>
2014-08-30 10:54:12 -0400

    Cleanup

--- src/main/java/com/timmattison/cryptocurrency/bitcoin/BitcoinBlockValidator.java
+++ src/main/java/com/timmattison/cryptocurrency/bitcoin/BitcoinBlockValidator.java
@@ -16,29 +16,32 @@ import java.util.Arrays;
  * To change this template use File | Settings | File Templates.
  */
 public class BitcoinBlockValidator implements BlockValidator {
-    private final HasherFactory hasherFactory;
     private final MerkleRootCalculator merkleRootCalculator;
 
     @Inject
-    public BitcoinBlockValidator(HasherFactory hasherFactory, MerkleRootCalculator merkleRootCalculator) {
-        this.hasherFactory = hasherFactory;
+    public BitcoinBlockValidator(MerkleRootCalculator merkleRootCalculator) {
         this.merkleRootCalculator = merkleRootCalculator;
     }
 
     @Override
     public boolean isValid(Block block) {
+        // Calculate the Merkle root of this block and make sure that it matches
         boolean merkleRootMatches = Arrays.equals(block.getBlockHeader().getMerkleRoot(), merkleRootCalculator.calculateMerkleRoot(block));
 
         if(!merkleRootMatches) {
+            // Doesn't match, fail
             return false;
         }
 
+        // Check to see if the hash is below the target
         boolean hashIsBelowTarget = block.getBlockHeader().getTarget().isBelowHash(block.getBlockHeader().getHashBigInteger());
 
         if(!hashIsBelowTarget) {
+            // Isn't below target, fail
             return false;
         }
 
+        // Looks good
         return true;
     }
 
