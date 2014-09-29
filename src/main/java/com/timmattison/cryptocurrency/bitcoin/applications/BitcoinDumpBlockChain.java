53863b792b3c244d8dd3d6cc83b90a313b666508
Tim Mattison <tim@mattison.org>
2014-09-29 08:46:13 -0400

    Added application to dump the block chain to a database without checking it

---
+++ src/main/java/com/timmattison/cryptocurrency/bitcoin/applications/BitcoinDumpBlockChain.java
@@ -0,0 +1,91 @@
+package com.timmattison.cryptocurrency.bitcoin.applications;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.timmattison.cryptocurrency.factories.BlockChainFactory;
+import com.timmattison.cryptocurrency.interfaces.Block;
+import com.timmattison.cryptocurrency.interfaces.BlockChain;
+import com.timmattison.cryptocurrency.modules.BitcoinModule;
+import com.timmattison.cryptocurrency.standard.interfaces.BlockStorage;
+import org.apache.commons.cli.ParseException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timmattison
+ * Date: 7/31/13
+ * Time: 5:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BitcoinDumpBlockChain {
+    private static final String APPLICATION_NAME = "BitcoinDumpBlockChain";
+    private static Logger logger;
+
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException, ParseException {
+        ApplicationHelper.logFine();
+
+        Map<String, String> options = ApplicationHelper.processCommandLineOptions(args, APPLICATION_NAME);
+
+        BitcoinModule bitcoinModule = new BitcoinModule();
+
+        if (options.containsKey(ApplicationHelper.BLOCKCHAIN)) {
+            bitcoinModule.useBlockChainFile(options.get(ApplicationHelper.BLOCKCHAIN));
+        }
+
+        String database = options.get(ApplicationHelper.DATABASE);
+        boolean mysql = Boolean.parseBoolean(options.get(ApplicationHelper.MYSQL));
+        boolean postgresql = Boolean.parseBoolean(options.get(ApplicationHelper.POSTGRESQL));
+
+        if (mysql) {
+            bitcoinModule.useMySqlStorage(database);
+        }
+
+        if (postgresql) {
+            bitcoinModule.usePostgresqlStorage(database);
+        }
+
+        Injector injector = Guice.createInjector(bitcoinModule);
+        logger = injector.getInstance(Logger.class);
+
+        BlockChain blockChain = injector.getInstance(BlockChainFactory.class).getBlockChain();
+        BlockStorage blockStorage = injector.getInstance(BlockStorage.class);
+
+        long blockNumber = 0;
+
+        int blockCount = blockStorage.getBlockCount();
+
+        if (blockCount > 0) {
+            long lastBlockNumber = blockStorage.getLastBlockNumber();
+            long lastBlockOffset = blockStorage.getBlockOffset(lastBlockNumber);
+
+            // Go to the last block we already processed
+            blockChain.skip(lastBlockOffset);
+
+            // Read the block so the next read skips over it
+            Block lastBlock = blockChain.next();
+
+            blockNumber = lastBlockNumber + 1;
+
+            logger.info("Starting at block number " + blockNumber + ", offset " + lastBlockOffset);
+        }
+
+        Block block = blockChain.next();
+
+        while (block != null) {
+            blockStorage.storeBlock(blockNumber, block);
+
+            Block fromDb = blockStorage.getBlock(blockNumber);
+
+            // Move onto the next block
+            block = blockChain.next();
+            if ((blockNumber % 1000) == 0) {
+                logger.info(String.valueOf(blockNumber));
+            }
+            blockNumber++;
+        }
+    }
+}
